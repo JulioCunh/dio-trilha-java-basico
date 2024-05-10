e206a9161d2cdb8c308999f70f4b785f8c617d01
Alumni <fcezarf@gmail.com>
2024-05-10 08:51:22 -0300

    tarefa concluída

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Digite seu numero do agencia: ");
+    Number agencia = scanner.nextInt();
+
+    System.out.println("Digite sua conta: ");
+    String conta = scanner.next();
+
+    System.out.println("Digite seu nome:");
+    String nomeCliente = scanner.next();
+
+    System.out.println("Digite seu nome:");
+    String sobreNome = scanner.next();
+
+    System.out.println("Qual o valor do deposito? ");
+    double saldo = scanner.nextDouble();
+
+    System.out.println(
+        "Olá " + nomeCliente + " " + sobreNome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+            + agencia + ", conta " + conta + " e seu saldo" + saldo + " já está disponível para saque");
+
+  }
+}
