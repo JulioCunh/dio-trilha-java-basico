da70f90550497abf8893163ed295087804c9b746
Julio Cunha <fcezarf@gmail.com>
2024-05-16 15:11:03 -0300

    refatoração

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -3,26 +3,29 @@ import java.util.Scanner;
 
 public class Contador {
   public static void main(String[] args) throws Exception {
+    Scanner terminal = new Scanner(System.in);
+    System.out.println("Digite o primeiro parâmetro");
+    int parametroUm = terminal.nextInt();
+    System.out.println("Digite o segundo parâmetro");
+    int parametroDois = terminal.nextInt();
+
     try {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Informe o primeiro parâmetro:");
-      int parameter1 = scanner.nextInt();
-      System.out.println("Informe o segundo parâmetro:");
-      int parameter2 = scanner.nextInt();
-      count(parameter1, parameter2);
+      contar(parametroUm, parametroDois);
+
     } catch (ParametrosInvalidosException e) {
       System.out.println(e.getMessage());
     } catch (InputMismatchException e) {
       System.out.println("Parametro inválido, informe um número inteiro.");
     }
+
   }
 
-  static void count(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
-    int interação = parametroDois - parametroUm;
-    for (int i = 1; i <= interação; i++) {
+  static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+    int interacao = parametroDois - parametroUm;
+    for (int i = 1; i < interacao; i++) {
       System.out.println("Imprimindo o número " + i);
     }
-    if (interação < 0)
+    if (interacao < 0)
       throw new ParametrosInvalidosException();
   }
 }
